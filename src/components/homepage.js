18fe57b9c8967e8fa25bd8771854593f2ea6f398
Mangirish <bhagatmangirish@gmail.com>
2023-05-24 17:22:30 +0530

    third commit

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,74 +3,92 @@ import React from "react";
 // import 'react-wheel-of-prizes/dist/index.css'
 import "./homepage.css";
 
-const HomePage = () => {
+class HomePage extends React.Component {
 
-    return (
-        <>
-            <div className="homepage">
+    state = {
+        name: "circle",
+    }
 
-                <div className="header">
-                    <h1> Spin-n-Win</h1>
+    startRotation = () => {
+        console.log("rotate")
+        this.setState({ name: "circle start-rotate" });
+
+        setTimeout(() => {
+            this.setState({ name: "circle start-rotate stop-rotate" });
+        }, Math.floor(Math.random() * 10000) + 1)
+    }
+
+    render() {
+        return (
+            <>
+                <div className="homepage">
+
+                    <div className="header">
+                        <h1> Spin-n-Win</h1>
+                    </div>
+
+                    <div className="arrow"> </div>
+
+                    <ul className={this.state.name}>
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 2 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 3 </div>
+                        </li>
+
+                        <li>
+                            <div className="text"> 1fyfhgfgh </div>
+                        </li>
+
+                    </ul>
+                    <div className="spin-Btn">
+                        <button onClick={this.startRotation} className="btn-Text">Spin</button>
+                    </div>
+                    {/* <div className="spin-btn">Spin</div> */}
                 </div>
+            </>
 
-                <ul className="circle">
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
+        )
+    }
 
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 1 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 2 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 3 </div>
-                    </li>
-
-                    <li>
-                        <div className="text"> 1fyfhgfgh </div>
-                    </li>
-
-                </ul>
-                <div className="spin-Btn">
-                    <button className="btn-Text">Spin</button>
-                </div>
-                {/* <div className="spin-btn">Spin</div> */}
-            </div>
-        </>
-
-    )
 }
 
 export default HomePage;
